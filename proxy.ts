f9dd521ed938f4ce1e55d7e91ad006cdc1d054e1
guntsadze <dimitri.guncadze@gmail.com>
2026-03-08 19:23:17 +0400

    [UPDATE]: logo

--- proxy.ts
+++ proxy.ts
@@ -40,5 +40,5 @@ export default function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\..*|api/auth).*)"],
+  matcher: ["/((?!_next/static|_next/image|logo.svg|.*\\..*|api/auth).*)"],
 };
