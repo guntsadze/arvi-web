2a3247684ff3f0bb513cda38632a1b9eb32bee6b
guntsadze <dimitri.guncadze@gmail.com>
2026-03-26 12:40:21 +0400

    [UPDATE[ robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -5,16 +5,10 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
-        disallow: ["/api/", "/auth/", "/settings/", "/messages/"],
-      },
-      {
-        userAgent: "Googlebot",
         allow: ["/", "/_next/static/"],
-        disallow: ["/api/", "/auth/", "/settings/", "/messages/"],
+        disallow: ["/settings/", "/messages/"],
       },
     ],
     sitemap: "https://arvi.ge/sitemap.xml",
-    host: "https://arvi.ge",
   };
 }
